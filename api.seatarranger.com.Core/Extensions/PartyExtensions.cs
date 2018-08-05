a3babe2b5515d330b957dd1d98d46406d5d5699c
Erik Zettersten <erik@zettersten.com>
2018-08-05 14:49:35 -0400

    new extensions

---
+++ api.seatarranger.com.Core/Extensions/PartyExtensions.cs
@@ -0,0 +1,42 @@
+﻿using api.seatarranger.com.Core.Models;
+
+namespace api.seatarranger.com.Core.Extensions
+{
+    public static class PartyExtensions
+    {
+        public static bool IsDislikedBy(this PartyEntity partyEntity, PartyEntity comparedEntity)
+        {
+            if (partyEntity.Dislikes == null)
+            {
+                return false;
+            }
+
+            if (!partyEntity.Dislikes.Contains(comparedEntity))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool IsEqualTo(this PartyEntity partyEntity, PartyEntity comparedEntity)
+        {
+            if (partyEntity.Equals(comparedEntity))
+            {
+                return true;
+            }
+
+            if (partyEntity.Name != comparedEntity.Name)
+            {
+                return false;
+            }
+
+            if (partyEntity.Size != comparedEntity.Size)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ api.seatarranger.com.Core/Extensions/TableExtensions.cs
@@ -0,0 +1,27 @@
+﻿using api.seatarranger.com.Core.Models;
+
+namespace api.seatarranger.com.Core.Extensions
+{
+    public static class TableExtensions
+    {
+        public static bool IsEqualTo(this TableEntity tableEntity, TableEntity comparedEntity)
+        {
+            if (tableEntity.Equals(comparedEntity))
+            {
+                return true;
+            }
+
+            if (tableEntity.Id != comparedEntity.Id)
+            {
+                return false;
+            }
+
+            if (tableEntity.Capacity != comparedEntity.Capacity)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
